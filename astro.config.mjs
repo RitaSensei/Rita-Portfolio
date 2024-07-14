79314c8c6b50a9bea73e85105a2d4f32404cce01
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-07-15 00:33:49 +0100

    chore: Update site and base URLs in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://RitaSensei.github.io",
+  base: "/Rita-Portfolio",
   integrations: [
     tailwind(),
     icon({
