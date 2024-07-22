ecd08ae3bb36920c208bf34820ed236f2f15a0fa
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-07-22 01:30:32 +0100

    refactor: Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://RitaSensei.github.io",
-  base: "/Rita-Portfolio",
   integrations: [
     tailwind(),
     icon({
