171bf81bc04b15266827cd17e944b216f3d171c7
Ghita <85077489+Ghita-L28@users.noreply.github.com>
2024-08-26 03:14:07 +0100

    refactor: Update links and 404 page

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
-export const SITE_URL: string = "https://rita.sensei.dev/";
-export const RESUME_LINK: string = "https://drive.google.com/file/d/1q9etbEZDoLIw5eaAz7edhJmUovYU8zNe/view?usp=sharing";
+export const SITE_URL: string = "https://rita-portfolio.vercel.app";
+export const RESUME_LINK: string = "/resume.pdf";
